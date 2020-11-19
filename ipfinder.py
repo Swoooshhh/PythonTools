c6d6d24531df87fac587dd2eabff63ba3acf019a
stalkerknot <57326660+stalkerknot@users.noreply.github.com>
2020-11-19 09:30:21 +0000

    Update ipfinder.py

--- ipfinder.py
+++ ipfinder.py
@@ -1,18 +1,16 @@
-who = input('Who would you like to search? no caps ')
-
-with open("test.txt") as ip:
+#To ask who to search
+who = input('Who would you like to search?') 
+#Open iplist.txt which contains the names and IP addresses
+with open("iplist.txt") as ip:
 	for line in ip.readlines():
 		if who in line:
 			print( line )
-		#else:
-			#print('nothing found')
-
-
+#Ask if they would like to add anyone.
 add = input('Would you like to add anyone? y/n ')
 if add == "y":
-	newname = input('name: ')
-	newip = input('ip: ')
-	with open('test.txt',mode='r+') as ip_write:
+	newname = input('Name: ')
+	newip = input('IP: ')
+	with open('iplist.txt',mode='r+') as ip_write:
 		ip_write.write(f'\n{newname} {newip}')
 else:
 	pass
